08d2cdee4110c2e2bfdad8dcdf41ec3053093248
Bryson Oakley <boakley@cricut.com>
2023-11-01 15:00:14 -0600

    Add more plugins and extensions to telescope

---
+++ lua/bryson/plugins/fugitive.lua
@@ -0,0 +1,8 @@
+return {
+    "tpope/vim-fugitive",
+    keys = {
+        { "<leader>gg", [[Git]], desc = "Git" },
+        { "<leader>gc", [[Git commit]], desc = "Git commit" },
+        { "<leader>gf", [[Git diff]], desc = "Git diff" }
+    }
+}
--- lua/bryson/plugins/telescope.lua
+++ lua/bryson/plugins/telescope.lua
@@ -1,16 +1,22 @@
 return {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.4",
-    dependencies = { 
+    dependencies = {
         "nvim-lua/plenary.nvim",
         { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-        "nvim-tree/nvim-web-devicons"
+        "nvim-tree/nvim-web-devicons",
+        "nvim-telescope/telescope-frecency.nvim",
+        "nvim-telescope/telescope-file-browser.nvim",
+        "nvim-telescope/telescope-project.nvim",
     },
     keys = {
         { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
         { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Find buffers" },
         { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Find help" },
         { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
+        { "<leader>fr", "<cmd>Telescope frecency<cr>", desc = "Frecency" },
+        { "<leader>fe", "<cmd>Telescope file_browser<cr>", desc = "File Browser" },
+        { "<leader>fp", "<cmd>lua require'telescope'.extensions.project.project{}<cr>", desc = "Projects" },
     },
     config = function()
         local telescope = require("telescope")
@@ -24,9 +30,24 @@ return {
                         ["<C-j>"] = actions.move_selection_next
                     }
                 }
+            },
+            extensions = {
+                project = {
+                    base_dirs = {
+                        "C:/Repos/DesignSpace",
+                        "~/AppData/Local/nvim/"
+                    },
+                    on_project_selected = function (buffnr)
+                        local project_actions = require("telescope._extensions.project.actions")
+                        project_actions.change_working_directory(buffnr, false)
+                    end
+                }
             }
         })
 
         telescope.load_extension("fzf")
+        telescope.load_extension("frecency")
+        telescope.load_extension("file_browser")
+        telescope.load_extension("project")
     end
 }
