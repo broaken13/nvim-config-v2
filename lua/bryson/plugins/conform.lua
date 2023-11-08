730e2c281de1405ea038c40299c1e319eda6642e
Bryson Oakley <broaken13@gmail.com>
2023-11-07 20:47:40 -0700

    Add formatting and autotagging

---
+++ lua/bryson/plugins/conform.lua
@@ -0,0 +1,22 @@
+-- Conform is a formatting plugin
+return {
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			javascript = { "prettier" },
+			typescript = { "prettier" },
+			typescriptreact = { "prettier" },
+		},
+	},
+	config = function(_, opts)
+		require("conform").setup(opts)
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			pattern = "*",
+			callback = function(args)
+				require("conform").format({ bufnr = args.buf })
+			end,
+		})
+	end,
+}
--- lua/bryson/plugins/treesitter.lua
+++ lua/bryson/plugins/treesitter.lua
@@ -1,20 +1,29 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    event = { "BufReadPre", "BufNewFile" },
-    build = ":TSUpdate",
-    config = function()
-        require'nvim-treesitter.configs'.setup {
-            ensure_installed = { 
-                "c_sharp",
-                "rust",
-                "typescript",
-                "javascript",
-                "lua",
-                "json" 
-            },
-            highlight = {
-                enable = true
-            }
-        }
-    end
+	"nvim-treesitter/nvim-treesitter",
+	dependencies = {
+		"windwp/nvim-ts-autotag", -- Auto close xml style tags
+	},
+	event = { "BufReadPre", "BufNewFile" },
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				"c_sharp",
+				"rust",
+				"typescript",
+				"javascript",
+				"lua",
+				"json",
+				"tsx",
+				"css",
+				"html",
+			},
+			highlight = {
+				enable = true,
+			},
+			autotag = {
+				enable = true,
+			},
+		})
+	end,
 }
