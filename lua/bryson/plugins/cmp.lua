b06a8013ae7bd3b6977c5474f9eb79894f1960fc
Bryson Oakley <broaken13@gmail.com>
2024-01-23 15:53:09 -0700

    Add tab jumping in snippets

--- lua/bryson/plugins/cmp.lua
+++ lua/bryson/plugins/cmp.lua
@@ -1,3 +1,9 @@
+local has_words_before = function()
+    unpack = unpack or table.unpack
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 return {
 	"hrsh7th/nvim-cmp",
 	event = "InsertEnter",
@@ -36,7 +42,25 @@ return {
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
 				["<C-Space>"] = cmp.mapping.complete(), --open completion menu
 				["<C-m>"] = cmp.mapping.abort(),
-				["<Tab>"] = cmp.mapping.confirm({ select = true }),
+				--["<Tab>"] = cmp.mapping.confirm({ select = true }),
+                ["<Tab>"] = cmp.mapping(function (fallback)
+                    if cmp.visible() then
+                        cmp.confirm({ select = true })
+                    elseif luasnip.expand_or_jumpable() then
+                        luasnip.expand_or_jump()
+                    elseif has_words_before() then
+                        cmp.complete()
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+                ["<S-Tab>"] = cmp.mapping(function (fallback)
+                    if luasnip.jumpable(-1) then
+                        luasnip.jump(-1)
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
 				["<CR>"] = cmp.mapping.confirm({ select = false }),
 			}),
 			sources = cmp.config.sources({
