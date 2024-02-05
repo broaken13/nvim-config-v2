e0d013e172d9a5f4d6ecc83874aa8a0309ed2947
Bryson Oakley <broaken13@gmail.com>
2024-02-05 12:38:37 -0700

    Make background transparent

--- lua/bryson/plugins/colorscheme.lua
+++ lua/bryson/plugins/colorscheme.lua
@@ -3,7 +3,20 @@ return {
 	"Shatur/neovim-ayu",
 	lazy = false,
 	priority = 1000,
-	config = function()
+    config = function()
+        require('ayu').setup({
+            overrides = {
+                Normal = { bg = "None" },
+                ColorColumn = { bg = "None" },
+                SignColumn = { bg = "None" },
+                Folded = { bg = "None" },
+                FoldColumn = { bg = "None" },
+                CursorLine = { bg = "None" },
+                CursorColumn = { bg = "None" },
+                WhichKeyFloat = { bg = "None" },
+                VertSplit = { bg = "None" },
+            },
+        })
 		vim.cmd([[colorscheme ayu-dark]])
 	end,
 }
