7be1c1e4a94a14d799b39a66971a87da33dd7051
Bryson Oakley <broaken13@gmail.com>
2024-10-07 19:07:51 -0600

    Add whichkey and precog

--- lua/bryson/plugins/telescope.lua
+++ lua/bryson/plugins/telescope.lua
@@ -1,10 +1,13 @@
 return {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
     -- or                              , branch = '0.1.x',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = { 'nvim-lua/plenary.nvim', "folke/which-key.nvim" },
     event = "VeryLazy",
     config = function()
         local builtin = require('telescope.builtin')
+        
+        local wk = require("which-key")
+        wk.add { "<leader>f", group = "Find" }
         vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
         vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
