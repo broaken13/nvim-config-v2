f1cfa8e70364aed849d4423d135a4ca535e53960
Bryson Oakley <broaken13@gmail.com>
2024-10-07 23:03:36 -0600

    Cmp initial pass

---
+++ lua/bryson/plugins/cmp.lua
@@ -0,0 +1,51 @@
+return {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+        "neovim/nvim-lspconfig",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline",
+    },
+    event = "VeryLazy",
+    config = function()
+        local cmp = require("cmp")
+
+        cmp.setup({
+            window = {
+                completion = cmp.config.window.bordered(),
+                documentation = cmp.config.window.bordered(),
+            },
+            sources = cmp.config.sources({
+                { name = "nvim-lsp" },
+            }, 
+            {
+                { name = "buffer" },
+            }),
+            mapping = cmp.mapping.preset.insert({
+                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                ["<C-Space>"] = cmp.mapping.complete(),
+                ["<C-e>"] = cmp.mapping.abort(),
+                ["<Tab>"] = cmp.mapping.confirm({ select = true }),
+            }),
+        })
+
+        cmp.setup.cmdline({ "/", "?" }, {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = {
+                { name = "buffer" }
+            }
+        })
+
+        cmp.setup.cmdline(":", {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = cmp.config.sources({
+                { name = "path" }
+            }, {
+                { name = "cmdline" }
+            }),
+            matching = { disallow_symbol_nonprefix_matching = false }
+        })
+    end
+}
