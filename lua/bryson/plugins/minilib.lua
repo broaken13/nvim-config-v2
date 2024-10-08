aff8e531ccf0340e7b93633ddc43ded639696908
Bryson Oakley <broaken13@gmail.com>
2024-10-07 19:43:17 -0600

    Minifiles

---
+++ lua/bryson/plugins/minilib.lua
@@ -0,0 +1,11 @@
+return {
+    "echasnovski/mini.nvim",
+    dependencies = { "folke/which-key.nvim", },
+    version = false,
+    config = function()
+        require("mini.icons").setup()
+        require("mini.files").setup()
+        local wk = require("which-key")
+        wk.add { "<leader>ee", function() MiniFiles.open() end, desc = "Mini Files" }
+    end
+}
