b00c7898d52c7fb0bda1d8efa155524a61d2ccd7
Bryson Oakley <broaken13@gmail.com>
2024-10-07 22:39:16 -0600

    Git stuff

---
+++ lua/bryson/plugins/git.lua
@@ -0,0 +1,30 @@
+return {
+    {
+        "NeogitOrg/neogit",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "sindrets/diffview.nvim",
+
+            "nvim-telescope/telescope.nvim",
+        },
+        opts = {
+            graph_style = "unicode",
+        },
+        keys = {
+            {
+                "<leader>gg",
+                function()
+                    require("neogit").open()
+                end,
+                desc = "Open Neogit",
+            },
+            { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Open Diffview" },
+        },
+    },
+    {
+        "lewis6991/gitsigns.nvim",
+        event = "VeryLazy",
+        opts = {}
+    }
+}
+
