071dd88d4415bdc25a65d616ced952a565a5d005
bryson.oakley <bryson.oakley@baesystems.us>
2025-01-15 12:14:51 -0700

    Add formatting entry for typescript

--- lua/bryson/plugins/conform.lua
+++ lua/bryson/plugins/conform.lua
@@ -16,6 +16,7 @@ return {
 		formatters_by_ft = {
 			lua = { "stylua" },
 			javascript = { "prettierd", "prettier", stop_after_first = true },
+			typescript = { "prettierd", "prettier", stop_after_first = true },
 			cs = { "csharpier" },
 		},
 	},
