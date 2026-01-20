e71195257b984e04f78a8955b7e6d4f47a41e18e
Bryson Oakley <broaken13@gmail.com>
2026-01-20 15:41:45 -0700

    Reset

--- lua/bryson/core.lua
+++ lua/bryson/core.lua
@@ -7,14 +7,32 @@ vim.opt.relativenumber = true
 vim.opt.cursorline = true
 vim.opt.termguicolors = true
 
+-- Sync clipboard with system
+vim.opt.clipboard = "unnamedplus"
+
+-- Confirm save when exiting with changes
+vim.opt.confirm = true
+
 -- Better tab behavior
 vim.opt.backspace = "indent,eol,start"
 vim.opt.expandtab = true
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
--- round to nearest increment of shiftwidth (in this case, 4)
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+-- round to nearest increment of shiftwidth (in this case, 2)
 vim.opt.shiftround = true
+vim.opt.smartindent = true
+vim.opt.list = true
 
+vim.opt.signcolumn = "yes"
+
+-- How many lines are kept at the bottom/top when scrolling
+vim.opt.sidescrolloff = 4
+
+-- Better search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.winminwidth = 5
 -- easier window nav
 vim.keymap.set({'n', 't'}, '<C-j>', '<cmd>wincmd j<cr>')
 vim.keymap.set({'n', 't'}, '<C-h>', '<cmd>wincmd h<cr>')
@@ -24,10 +42,3 @@ vim.keymap.set({'n', 't'}, '<C-l>', '<cmd>wincmd l<cr>')
 -- clear search highlight with esc
 vim.keymap.set('n', '<esc>', '<cmd>nohl<cr>')
 
-function _G.set_terminal_keymaps()
-    local opts = {buffer = 0}
-    vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-    vim.keymap.set('t', '<C-\\>', '<cmd>ToggleTerm<cr>')
-end
-
-vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
--- lua/bryson/plugins/toggleterm.lua
+++
@@ -1,7 +0,0 @@
-return {
-	'akinsho/toggleterm.nvim',
-	opts = {},
-	keys = {
-		{ '<C-\\>', '<cmd>ToggleTerm<cr>', desc = 'Toggle term' },
-	},
-}
