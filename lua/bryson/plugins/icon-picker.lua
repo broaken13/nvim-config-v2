f942b4c12bfb625afa2aa9177f3ba9c6e8d5cd92
Bryson Oakley <broaken13@gmail.com>
2026-01-23 17:26:06 -0700

    Picker and way to select icons

---
+++ lua/bryson/plugins/icon-picker.lua
@@ -0,0 +1,15 @@
+return {
+    "ziontee113/icon-picker.nvim",
+    config = function()
+        require("icon-picker").setup({ disable_legacy_commands = true })
+
+        local opts = { noremap = true, silent = true }
+
+        opts.desc = "Icon picker"
+        vim.keymap.set("n", "<Leader>fi", "<cmd>IconPickerNormal<cr>", opts)
+        opts.desc = "Icon picker to register"
+        vim.keymap.set("n", "<Leader><Leader>y", "<cmd>IconPickerYank<cr>", opts) --> Yank the selected icon into register
+        opts.desc = "Icon picker (insert mode)"
+        vim.keymap.set("i", "<C-t>", "<cmd>IconPickerInsert<cr>", opts)
+    end
+}
---
+++ lua/bryson/plugins/snacks.lua
@@ -0,0 +1,10 @@
+return {
+  "folke/snacks.nvim",
+  -- Just after colorscheme
+  priority = 999,
+  lazy = false,
+  opts = {
+    input = { enabled = true },
+    picker = { enabled = true },
+  }
+}
