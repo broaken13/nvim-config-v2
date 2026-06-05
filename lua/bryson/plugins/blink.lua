690ea4772d9bf529cd1e4fca229e46b38e539f61
Oakley <bryson.oakley@baesystems.us>
2026-06-05 10:47:11 -0600

    Add LazyDev

--- lua/bryson/plugins/blink.lua
+++ lua/bryson/plugins/blink.lua
@@ -1,4 +1,15 @@
 return {
+  {
+    "folke/lazydev.nvim",
+    ft = "lua",
+    opts = {
+      library = {
+        "lazy.nvim",
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+      }
+    }
+  },
+{
   'saghen/blink.cmp',
   -- optional: provides snippets for the snippet source
   dependencies = { 'rafamadriz/friendly-snippets' },
@@ -39,7 +50,14 @@ return {
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
+      default = { 'lazydev', 'lsp', 'path', 'snippets', 'buffer' },
+      providers = {
+        lazydev = {
+          name = "LazyDev",
+          module = "lazydev.integrations.blink",
+          score_offset = 100,
+        }
+      }
     },
 
     -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
@@ -47,7 +65,7 @@ return {
     -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
     --
     -- See the fuzzy documentation for more information
-    fuzzy = { implementation = "prefer_rust_with_warning" }
+    fuzzy = { implementation = "lua" }
   },
   opts_extend = { "sources.default" }
-}
+}}
